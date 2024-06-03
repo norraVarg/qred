c43d922ef585a9a458564183109475a340e8882d
norraVarg <wolf.se2015@gmail.com>
2024-06-03 10:32:53 +0200

    use scroll outside for nav menu

--- app/ui/nav-bar.tsx
+++ app/ui/nav-bar.tsx
@@ -12,24 +12,32 @@ const NAV_ITEMS = [
 ]
 
 const NavBar = () => {
-  const [showUserMenu, setShowMenu] = useState(false)
+  const [showAccountMenu, setAccountMenu] = useState(false)
   const [showNavMenu, setShowNavMenu] = useState(false)
   const pathname = usePathname()
 
-  const userMenuRef = useRef(null)
-  const hideUserMenu = () => {
-    if (showUserMenu) {
-      setShowMenu(false)
+  const accountMenuRef = useRef(null)
+  const hideAccountMenu = () => {
+    if (showAccountMenu) {
+      setAccountMenu(false)
     }
   }
-  useClickOutside(userMenuRef, hideUserMenu)
+  useClickOutside(accountMenuRef, hideAccountMenu)
+
+  const navMenuRef = useRef(null)
+  const hideNavMenu = () => {
+    if (showNavMenu) {
+      setShowNavMenu(false)
+    }
+  }
+  useClickOutside(navMenuRef, hideNavMenu)
 
   const toggleNavMenu = () => {
     setShowNavMenu(!showNavMenu)
   }
 
-  const toggleUserMenu = () => {
-    setShowMenu(!showUserMenu)
+  const toggleAccountMenu = () => {
+    setAccountMenu(!showAccountMenu)
   }
 
   return (<nav className="bg-sky-500">
@@ -55,19 +63,19 @@ const NavBar = () => {
             </div>
           </div>
         </div>
-        <div ref={userMenuRef} className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+        <div ref={accountMenuRef} className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
           <div className="relative ml-3">
-            <button onClick={toggleUserMenu} type="button" className="relative inline-flex items-center justify-center rounded-md p-1 text-white hover:bg-sky-700 hover:text-white focus:outline-none focus:ring-1 focus:ring-inset focus:ring-white" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+            <button onClick={toggleAccountMenu} type="button" className="relative inline-flex items-center justify-center rounded-md p-1 text-white hover:bg-sky-700 hover:text-white focus:outline-none focus:ring-1 focus:ring-inset focus:ring-white" id="account-menu-button" aria-expanded="false" aria-haspopup="true">
               <span className="absolute -inset-1.5"></span>
-              <span className="sr-only">Open user menu</span>
+              <span className="sr-only">Open account menu</span>
               <svg className="fill-current size-5 stroke-white stroke-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
               </svg>
             </button>
 
-            {showUserMenu && (
-              <div onClick={hideUserMenu} className="absolute right-0 z-10 mt-1 w-48 origin-top-right divide-y divide-gray-200 rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex={-1}>
-                <Link href="/admin/account" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-0">Account</Link>
+            {showAccountMenu && (
+              <div onClick={hideAccountMenu} className="absolute right-0 z-10 mt-1 w-48 origin-top-right divide-y divide-gray-200 rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="account-menu-button" tabIndex={-1}>
+                <Link href="/admin/account" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1}>Account</Link>
                 <button className="flex w-full px-4 py-2 text-sm text-gray-700"
                   onClick={() => signOutAction()}>Sign out</button>
               </div>
@@ -78,13 +86,12 @@ const NavBar = () => {
     </div>
 
     {showNavMenu && (
-      <div className="space-y-1 p-2">
+      <div ref={navMenuRef} className="space-y-1 p-2">
         {NAV_ITEMS.map((item) => (
-          <Link onClick={() => setShowNavMenu(false)} key={item.name} href={item.href} className={`text-white hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium ${pathname === item.href ? 'bg-sky-700' : ''}`}> {item.name}</Link>
+          <Link onClick={() => setShowNavMenu(false)} href={item.href} key={item.name} className={`text-white hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium ${pathname === item.href ? 'bg-sky-700' : ''}`}> {item.name}</Link>
         ))}
       </div>
-    )
-    }
+    )}
   </nav >
   )
 }
--- app/ui/users-list.tsx
+++ app/ui/users-list.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { QredUser } from '../lib/definitions'
 import Link from 'next/link'
 
@@ -14,13 +13,11 @@ const UsersList = (props: Props) => {
     <ul role="list" className="row-start-2 row-span-10 divide-y divide-gray-200 overflow-y-auto">
       {users.map(user => (
         <Link href={`/admin/user-details?id=${user.id}`} key={user.id} className="flex justify-between gap-x-6 py-4">
-          <div className="flex min-w-0 gap-x-4">
-            <div className="min-w-0 flex-auto">
-              <p className="text-xs font-semibold leading-6 text-gray-900">{user.name}</p>
-              <p className="mt-1 truncate text-xs leading-5 text-gray-500">Company: {user.company.name}</p>
-              <p className="mt-1 truncate text-xs leading-5 text-gray-500">Email: {user.email}</p>
-              <p className="mt-1 truncate text-xs leading-5 text-gray-500">Phone: {user.phone}</p>
-            </div>
+          <div className="flex flex-col gap-1">
+            <p className="text-xs font-semibold leading-6 text-gray-900">{user.name}</p>
+            <p className="truncate text-xs leading-5 text-gray-500">Company: {user.company.name}</p>
+            <p className="truncate text-xs leading-5 text-gray-500">Email: {user.email}</p>
+            <p className="truncate text-xs leading-5 text-gray-500">Phone: {user.phone}</p>
           </div>
         </Link>
       ))}
